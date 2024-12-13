9192b1f0052f260726dd64917caba4f4c3995c7c
Flavio Magalhaes <flaviomg9@gmail.com>
2024-12-13 22:17:32 +0000

    Menu atualizado

--- src/main/java/tetronix/Control/InputHandlerForMenu.java
+++ src/main/java/tetronix/Control/InputHandlerForMenu.java
@@ -7,20 +7,31 @@ import tetronix.Model.Menu;
 import static tetronix.Model.MenuState.PLAYING;
 import static tetronix.Model.MenuState.STATISTICS;
 
-public class InputHandlerForMenu implements InputHandler{
-    private Menu menu;
+public class InputHandlerForMenu implements InputHandler {
+    private final Menu menu;
 
-    public InputHandlerForMenu(Menu menu_){
+    public InputHandlerForMenu(Menu menu_) {
         this.menu = menu_;
     }
 
-
     @Override
-    public void processInput(KeyStroke key){
-
+    public void processInput(KeyStroke key) {
         switch (key.getKeyType()) {
+            case ArrowUp:
+
+                menu.setSelectedOption((menu.getSelectedOption() > 0)
+                        ? menu.getSelectedOption() - 1
+                        : menu.getOptions().size() - 1);
+                break;
+            case ArrowDown:
+
+                menu.setSelectedOption((menu.getSelectedOption() < menu.getOptions().size() - 1)
+                        ? menu.getSelectedOption() + 1
+                        : 0);
+                break;
             case Enter:
-                menu.setCurr_state(PLAYING);
+
+                handleSelection();
                 break;
             case Escape:
                 System.exit(0);
@@ -33,6 +44,21 @@ public class InputHandlerForMenu implements InputHandler{
                     menu.setCurr_state(STATISTICS);
                 }
                 break;
+        }
+    }
+
+    private void handleSelection() {
+        String selectedOption = menu.getOptions().get(menu.getSelectedOption());
+        switch (selectedOption) {
+            case "Start Game":
+                menu.setCurr_state(PLAYING);
+                break;
+
+            case "Exit":
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Unknown option!");
         }
     }
 }
--- src/main/java/tetronix/Model/Menu.java
+++ src/main/java/tetronix/Model/Menu.java
@@ -13,20 +13,23 @@ import java.util.List;
 import static tetronix.Model.MenuState.*;
 
 public class Menu {
-    private ScreenManager screenManager; //mais tarde vai passar como argumento para o game
+    private ScreenManager screenManager;
     private Game game;
     private MenuState curr_state;
     private ElementViewer<Menu> menuView;
     private ElementViewer<Menu> menuGame_OverView;
     private ElementViewer<Menu> menuStatisticsView;
     private InputHandler menuController;
-    List<Integer> Track_Scores = new ArrayList<>();
-    private int rows = 40;
-    private  int columns = 40;
 
+    private final List<String> options = List.of("Start Game", "Exit");
+    private int selectedOption = 0;
+
+    List<Integer> Track_Scores = new ArrayList<>();
+    private int rows = 20;
+    private int columns = 40;
 
     public Menu() throws IOException {
-        screenManager = new ScreenManager(rows,columns);
+        screenManager = new ScreenManager(rows, columns);
         menuView = new MenuView(this);
         menuGame_OverView = new MenuGame_OverView(screenManager);
         menuStatisticsView = new MenuStatisticsView(this);
@@ -34,17 +37,36 @@ public class Menu {
         curr_state = INITIAL_MENU;
     }
 
-    public List<Integer> getTrack_Scores() {return Track_Scores;}
+    public List<Integer> getTrack_Scores() {
+        return Track_Scores;
+    }
 
-    public void setCurr_state(MenuState curr_state_) {this.curr_state = curr_state_;}
+    public void setCurr_state(MenuState curr_state_) {
+        this.curr_state = curr_state_;
+    }
 
-    public MenuState getCurr_state() {return curr_state;}
+    public MenuState getCurr_state() {
+        return curr_state;
+    }
 
-    public ScreenManager getScreenManager() {return screenManager;}
+    public ScreenManager getScreenManager() {
+        return screenManager;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public int getSelectedOption() {
+        return selectedOption;
+    }
+
+    public void setSelectedOption(int selectedOption) {
+        this.selectedOption = selectedOption;
+    }
 
     public void run() throws IOException {
         while (true) {
-
             switch (curr_state) {
                 case INITIAL_MENU:
                     menuView.draw();
@@ -54,7 +76,7 @@ public class Menu {
                 case PLAYING:
                     game = new Game(this);
                     game.run();
-                    Track_Scores.add(game.getScore() + game.get_Additional_Points());
+                    Track_Scores.add(game.getScore());
                     break;
                 case GAME_OVER:
                     menuGame_OverView.draw();
@@ -67,18 +89,12 @@ public class Menu {
                     screenManager.refresh();
                     handleInput();
                     break;
-
             }
         }
     }
-
-
 
     public void handleInput() throws IOException {
         KeyStroke key = screenManager.readInput();
         menuController.processInput(key);
     }
 }
-
-
-
