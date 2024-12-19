7ccf76ee025a56ef072a405cab43e2e11d391023
ArturMoreira <arturmoreira899@gmail.com>
2024-12-19 00:32:31 +0000

    little change

--- src/main/java/tetronix/View/NextBlockView.java
+++ src/main/java/tetronix/View/NextBlockView.java
@@ -47,15 +47,27 @@ public class NextBlockView implements ElementViewer<TetrisBlock> {
         int[][] shape = nextBlock.getShape();
         String color = nextBlock.getColor();
 
+        // Dimensões do retângulo branco
+        int rectWidth = 10;
+        int rectHeight = 6;
+
+        // Dimensões do bloco
+        int blockWidth = shape[0].length * 2; // Cada célula tem largura 2
+        int blockHeight = shape.length;
+
+        // Cálculo para centralizar o bloco dentro do retângulo
+        int offsetX = previewPosition.getRow_identifier() + (rectHeight - blockHeight) / 2;
+        int offsetY = previewPosition.getColumn_identifier() + (rectWidth - blockWidth) / 2;
+
         graphics.setBackgroundColor(TextColor.Factory.fromString(color));
 
-        // Renderiza o bloco usando a posição fornecida
+        // Renderiza o bloco centralizado
         for (int r = 0; r < shape.length; r++) {
             for (int c = 0; c < shape[0].length; c++) {
                 if (shape[r][c] == 1) {
-                    int x = previewPosition.getColumn_identifier() + c * 2;
-                    int y = previewPosition.getRow_identifier() + r;
-                    graphics.fillRectangle(new TerminalPosition(x, y), new TerminalSize(2, 1), ' ');
+                    int y = offsetY + c * 2; // Colunas precisam de multiplicação por 2
+                    int x = offsetX + r;     // Linhas são incrementadas diretamente
+                    graphics.fillRectangle(new TerminalPosition(y - 1, x - 1), new TerminalSize(2, 1), ' ');
                 }
             }
         }
